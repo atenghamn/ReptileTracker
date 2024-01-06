dcf7a766ee1690a6d12e28bfb096752bd45fb3c3
atenghamn <alex.tenghamn@gmail.com>
2024-01-06 15:01:48 +0100

    Added rate limiting

--- ReptileTrackerTests/obj/Debug/net8.0/ReptileTrackerTests.AssemblyInfo.cs
+++ ReptileTrackerTests/obj/Debug/net8.0/ReptileTrackerTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReptileTrackerTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c38d7bced0e8580b40e7fd8c7fce33357a17763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReptileTrackerTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReptileTrackerTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
